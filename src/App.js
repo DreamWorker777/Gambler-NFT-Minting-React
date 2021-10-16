04103eaf942d5a4394ae9640b4824bc3fca83b7f
dreamworker <devmetaverse34@gmail.com>
2021-10-16 20:48:49 +0300

    loading_page & change title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {NotificationContainer} from 'react-notifications';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
 	BrowserRouter as Router,
 	Switch,
@@ -17,7 +17,32 @@ import Footer from './pages/Footer/Footer';
 import TopMenu from './pages/TopMenu/TopMenu';
 import Join from './pages/Join/Join';
 
+import LoadingImg from './assets/img/tags/5.png';
+
+const Loading = ({ isLoading }) => {
+	return (
+		<div className={`loading ${ isLoading ? '' : 'fade-hide' }`}>
+			<img alt="loading" src={ LoadingImg }></img>
+
+			<p>Loading...</p>
+		</div>
+	)
+}
+
 function App() {
+	const [isLoading, setIsLoading] = useState(true);
+	const [showLoading, setShowLoading] = useState(true);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setIsLoading(false);
+			
+			setTimeout(() => {
+				setShowLoading(false);
+			}, 1000)
+		}, 1000);
+	})
+
 	return (
 		<Router>
 			<div className="App">
@@ -32,6 +57,8 @@ function App() {
 				<Footer />
 				<NotificationContainer />
 			</div>
+
+			{ showLoading ? <Loading isLoading={isLoading}/> : null}
 		</Router>
 	);
 }
