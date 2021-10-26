db3f9133ac4bf2b2e30bef7640dc49381773d4cd
dreamworker <devmetaverse34@gmail.com>
2021-10-26 23:56:05 +0300

    mint now page

---
+++ src/components/Mint/MintNow/MintNow.js
@@ -0,0 +1,127 @@
+import {
+    NavLink,
+} from "react-router-dom";
+
+import './MintNow.scss';
+
+import arrowLeft from '../../../assets/img/icons/arrow_left.svg';
+
+import title from '../../../assets/img/font_svg/mint_title_congratulations.svg';
+import tag from '../../../assets/img/tags/5.svg';
+import pic from '../../../assets/img/mint/mint_avatar.png';
+
+import skeleton from '../../../assets/img/icons/skeleton_white.svg';
+
+export const MintNow = () => {
+    return (
+        <div>
+            <div className="mint__backHome">
+                <NavLink to={{ pathname: '/' }}>
+                    <img alt="icon" src={ arrowLeft }></img>
+                    Back to home 
+                </NavLink>
+            </div>
+
+            <section className="mint__mintNow">
+                <div className="mint__mintNow__main">
+                    <div className="mint__mintNow__main__title">
+                        <img alt="title" src={title}></img>
+                        <p>YOU’VE BEEN SELECTED TO MINT YOUR CYBERUNNERS NFT</p>
+                    </div>
+
+                    <a href="#javascript;" className="mint__mintNow__main__button">
+                        MINT MY CYBERUNNERS - 0.35 ETH
+                    </a>
+
+                    <div className="mint__mintNow__main__timer">
+                        <div className="mint__mintNow__main__timer__desc">
+                            Estimated Wait Time
+                        </div>
+                        <div className="mint__mintNow__main__timer__wrapper">
+                            <div className="mint__mintNow__main__timer__value">
+                                <p className="mint__mintNow__main__timer__value__number"> 00 </p>
+                            </div>
+
+                            <div className="mint__mintNow__main__timer__split">
+                                <p>:</p>
+                            </div>
+
+                            <div className="mint__mintNow__main__timer__value">
+                                <p className="mint__mintNow__main__timer__value__number"> 00 </p>
+                            </div>
+
+                            <div className="mint__mintNow__main__timer__split">
+                                <p>:</p>
+                            </div>
+
+                            <div className="mint__mintNow__main__timer__value">
+                                <p className="mint__mintNow__main__timer__value__number"> 00 </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="mint__mintNow__pic">
+                    <div className="mint__mintNow__pic__wrapper">
+                        <img className="mint__mintNow__pic__tag" alt="pic" src={tag}></img>
+                        <img className="mint__mintNow__pic__main" alt="pic" src={pic}></img>
+                    </div>
+
+                    <div className="mint__mintNow__pic__ballance">
+                        <div className="mint__mintNow__pic__ballance__icon">
+                            <img alt="tag" src={skeleton}></img>
+                        </div>
+
+                        <div className="mint__mintNow__pic__ballance__detail">
+                            <div className="mint__mintNow__pic__ballance__detail__number">
+                                105 / 9,000
+                            </div>
+
+                            <div className="mint__mintNow__pic__ballance__detail__info">
+                                CYBERUNNERS ALREADY MINTED
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="mint__mintNow__timer">
+                    <div className="container">
+                        <div className="mint__mintNow__timer__tag">
+                            <img alt="tag" src={tag}></img>
+                        </div>
+
+                        <div className="mint__mintNow__timer__desc">
+                            Estimated Wait Time
+                        </div>
+                        <div className="mint__mintNow__timer__wrapper">
+                            <div className="mint__mintNow__timer__value">
+                                <p className="mint__mintNow__timer__value__desc"> DAYS </p>
+                                <p className="mint__mintNow__timer__value__number"> 00 </p>
+                            </div>
+
+                            <div className="mint__mintNow__timer__split">
+                                <p>:</p>
+                            </div>
+
+                            <div className="mint__mintNow__timer__value">
+                                <p className="mint__mintNow__timer__value__desc"> HOURS </p>
+                                <p className="mint__mintNow__timer__value__number"> 00 </p>
+                            </div>
+
+                            <div className="mint__mintNow__timer__split">
+                                <p>:</p>
+                            </div>
+
+                            <div className="mint__mintNow__timer__value">
+                                <p className="mint__mintNow__timer__value__desc"> MINUTES </p>
+                                <p className="mint__mintNow__timer__value__number"> 00 </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default MintNow;
--- src/pages/Mint/Mint.js
+++ src/pages/Mint/Mint.js
@@ -5,6 +5,7 @@ import {
 
 import Create from '../../components/Mint/Create/Create';
 import Confirmation from "../../components/Mint/Confirmation/Confirmation";
+import MintNow from '../../components/Mint/MintNow/MintNow';
 
 import './Mint.scss';
 
@@ -15,6 +16,7 @@ export const Mint = () => {
                 <Switch>
                     <Route exact path='/mint/create' component={Create} />
                     <Route exact path='/mint/confirm' component={Confirmation} />
+                    <Route exact path='/mint/mintNow' component={MintNow} />
                 </Switch>
             </div>
         </div>
