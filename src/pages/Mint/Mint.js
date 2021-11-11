3c1c4f162ec6345e91fb6b44e46b09d8a211ca1f
dreamworker <devmetaverse34@gmail.com>
2021-11-12 02:34:38 +0300

    state component

--- src/pages/Mint/Mint.js
+++ src/pages/Mint/Mint.js
@@ -10,18 +10,34 @@ import Sorry from '../../components/Mint/Sorry/Sorry';
 import Success from '../../components/Mint/Success/Success';
 
 import './Mint.scss';
+import { useState } from "react";
 
 export const Mint = () => {
+    // status => component
+    //       0 : create
+    //       1 : confirmation
+    //       2 : mintnow
+    //       3 : sorry
+    //       4 : success
+    const [status, setStatus] = useState(0);
+
     return (
         <div className="container">
             <div className="mint">
-                <Switch>
+                {
+                    status === 0 ? <Create /> :
+                    status === 1 ? <Confirmation /> :
+                    status === 2 ? <MintNow /> :
+                    status === 3 ? <Sorry /> : <Success />
+                }
+
+                {/* <Switch>
                     <Route exact path='/mint/create' component={Create} />
                     <Route exact path='/mint/confirm' component={Confirmation} />
                     <Route exact path='/mint/mintNow' component={MintNow} />
                     <Route exact path='/mint/sorry' component={Sorry} />
                     <Route exact path='/mint/success' component={Success} />
-                </Switch>
+                </Switch> */}
             </div>
         </div>
     )
